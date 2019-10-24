d885a665b0cc1889ebc652164c1b4abfb5042ec5
herlina23 <frois.herlina@gmail.com>
2019-10-24 16:42:18 +0700

    use HitReader

--- src/main/scala/Elas2.scala
+++ src/main/scala/Elas2.scala
@@ -1,79 +1,134 @@
-////import java.time.LocalDate
-////
-////import akka.http.documenteddsl.directives.UnmarshallingDDirectives._
-////import akka.http.documenteddsl.documentation.OutDocumentation._
-////import akka.http.documenteddsl.documentation.{JsonSchema, OutDocumentation, RouteDocumentation}
-////import akka.http.scaladsl.model.StatusCodes
-////import akka.http.scaladsl.testkit.ScalatestRouteTest
-////import com.fasterxml.jackson.databind.jsonschema.JsonSchema
-////import org.scalatest.MustMatchers._
-////import org.scalatest.WordSpec
-////import play.api.libs.json.{Format, Json}
-////
-////class UnmarshallingDDirectivesSpec extends WordSpec with DDirectivesSpec with ScalatestRouteTest {
-////  import UnmarshallingDDirectivesSpec._
-////
-////  "Out" must {
-////    val now = LocalDate.now()
-////
-////    "be applied to route documentation" in {
-////      Out[TestOut].describe(RouteDocumentation()).out mustBe Some(OutDocumentation(
-////        success = List(
-////          Payload.Success(
-////            status = Status(StatusCodes.OK),
-////            contentType = "application/json",
-////            schema = JsonSchema.resolveSchema[TestOut],
-////            example = None))))
-////    }
-////    "be applied to route documentation (concatenated)" in {
-////      val out = Out(StatusCodes.Created, TestOut("id", Some("name"), now)) & Out(StatusCodes.NotFound, "entity not found")
-////      out.describe(RouteDocumentation()).out mustBe Some(OutDocumentation(
-////        failure = List(
-////          Payload.Failure(
-////            status = Status(StatusCodes.NotFound),
-////            contentType = None,
-////            description = Some("entity not found"))),
-////        success = List(
-////          Payload.Success(
-////            status = Status(StatusCodes.Created),
-////            contentType = "application/json",
-////            schema = JsonSchema.resolveSchema[TestOut],
-////            example = Some(Json toJson TestOut("id", Some("name"), now))))))
-////    }
-////  }
-////
-////}
-////
-////object UnmarshallingDDirectivesSpec {
-////  case class TestOut(id: String, name: Option[String], createdAt: LocalDate)
-////  implicit val testInFormat: Format[TestOut] = Json.format[TestOut]
-////}
-//
-//
-//
-//import com.sksamuel.elastic4s.{Hit, HitReader}
-//
-//
-//// the other case class
-//
-//case class Character(name: String, location: String)
-//
-//implicit object CharacterHitReader extends HitReader[Character] {
-//  override def read(hit: Hit): Either[Throwable, Character] = {
-//    Right(Character(hit.sourceAsMap("name").toString, hit.sourceAsMap("location").toString))
-//  }
-//}
-//
-////https://stackoverflow.com/questions/49752112/avoid-await-in-elastic4s-query?rq=1
-////https://stackoverflow.com/questions/40517893/elastic4s-deserializing-search-results
-//
-//
-////https://static.javadoc.io/com.sksamuel.elastic4s/elastic4s-core_2.11/5.2.0/index.html#com.sksamuel.elastic4s.Hit
-//
-////val resp = client.execute {
-////  search("gameofthrones" / "characters").query("kings landing")
-////}.await // don't block in real code
-////
-////// .to[Character] will look for an implicit HitReader[Character] in scope
-////// and then convert all the hits into Characters for us.
-////val characters :Seq[Character] = resp.to[Character]
+import com.sksamuel.elastic4s.{ElasticsearchClientUri, Hit, HitReader, TcpClient}
+import com.sksamuel.elastic4s.http.ElasticDsl._
+import org.elasticsearch.action.support.WriteRequest.RefreshPolicy
+import com.sksamuel.elastic4s.http.HttpClient
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
+import com.sksamuel.elastic4s.searches.queries.matches.MatchQueryBuilder
+
+
+//cari pembenaran dari import yang masih merah
+
+
+object Elas2 extends App {
+  // Here we create an instance of the TCP client
+  val client = HttpClient(ElasticsearchClientUri("localhost",9200),(requestConfigBuilder: RequestConfig.Builder) => { requestConfigBuilder.setConnectionRequestTimeout(60000) .setSocketTimeout(60000) .setConnectTimeout(60000) }, (httpClientBuilder: HttpAsyncClientBuilder) => { httpClientBuilder })
+
+  case class ABC(aa: String, bb: String)
+  implicit object ABCHitReader extends HitReader[ABC] {
+    override def read(hit: Hit): Either[Throwable, ABC] = {
+      Right(ABC(hit.sourceAsMap("aa").toString, hit.sourceAsMap("bb").toString))
+    }
+  }
+
+
+  // https://stackoverflow.com/questions/40517893/elastic4s-deserializing-search-results
+
+  // now we can search for the document we just indexed
+  val resp = client.execute {
+    search("zox" / "test1") query "lalal"
+  }.await
+  val abc_new: Seq[ABC] = resp.to[ABC]
+
+//  case class Rassasa(rs:Result,pg:Paging)
+ case class ResultFill(
+                               id: Long,
+                               resource_type: String,
+                               specific_resource_type: String,
+                               stream_type: String,
+                               from_id: String,
+                               from_name: String,
+                               from_display_picture: String,
+                               content: String,
+                               final_sentiment: String,
+                               timestamp: String,
+                               object_content: String)
+  implicit object ResultFillReader extends HitReader[ResultFill] {
+    override def read(hit: Hit): Either[Throwable, ResultFill] = {
+      Right(ResultFill(
+        hit.sourceAsMap("id").toString.toLong,
+        hit.sourceAsMap("resource_type").toString,
+        hit.sourceAsMap("specific_resource_type").toString,
+        hit.sourceAsMap("stream_type").toString,
+        hit.sourceAsMap("from_id").toString,
+        hit.sourceAsMap("from_name").toString,
+        hit.sourceAsMap("from_display_picture").toString,
+        hit.sourceAsMap("content").toString,
+        hit.sourceAsMap("final_sentiment").toString,
+        hit.sourceAsMap("timestamp").toString,
+        hit.sourceAsMap("object_content").toString,
+      ))
+    }
+  }
+
+  case class Paging(next:String)
+  implicit object PagingReader extends HitReader[Paging] {
+    override def read(hit: Hit): Either[Throwable, Paging] = {
+      Right(Paging(hit.sourceAsMap("next").toString))
+    }
+  }
+   case class Result(results: List[ResultFill])
+  implicit object ResultHitReader extends HitReader[Result] {
+    override def read(hit: Hit): Either[Throwable, Result] = {
+      val obj = Result(
+        hit.sourceField("results").asInstanceOf[List[AnyRef]].map { entry =>
+          ResultFill(
+            hit.sourceAsMap("results.id").toString.toLong,
+            hit.sourceAsMap("results.resource_type").toString,
+            hit.sourceAsMap("results.specific_resource_type").toString,
+            hit.sourceAsMap("results.stream_type").toString,
+            hit.sourceAsMap("results.from_id").toString,
+            hit.sourceAsMap("results.from_name").toString,
+            hit.sourceAsMap("results.from_display_picture").toString,
+            hit.sourceAsMap("results.content").toString,
+            hit.sourceAsMap("results.final_sentiment").toString,
+            hit.sourceAsMap("results.timestamp").toString,
+            hit.sourceAsMap("results.object_content").toString,
+          )
+        }
+      )
+      Right(obj)
+    }
+  }
+
+// case class PagingFormat(paging:Option[Paging])
+
+  case class RawStream(result: List[ResultFill],paging:Paging)
+
+  implicit object RawStreamHitReader extends HitReader[RawStream] {
+    override def read(hit: Hit): Either[Throwable, RawStream] = {
+      val obj = RawStream(
+        hit.sourceField("result").asInstanceOf[List[AnyRef]].map { entry =>
+          ResultFill(
+            hit.sourceAsMap("result.id").toString.toLong,
+            hit.sourceAsMap("result.resource_type").toString,
+            hit.sourceAsMap("result.specific_resource_type").toString,
+            hit.sourceAsMap("result.stream_type").toString,
+            hit.sourceAsMap("result.from_id").toString,
+            hit.sourceAsMap("result.from_name").toString,
+            hit.sourceAsMap("result.from_display_picture").toString,
+            hit.sourceAsMap("result.content").toString,
+            hit.sourceAsMap("result.final_sentiment").toString,
+            hit.sourceAsMap("result.timestamp").toString,
+            hit.sourceAsMap("result.object_content").toString,
+          )
+        },
+        Paging(hit.sourceField("paging.next").toString),
+      )
+      Right(obj)
+    }
+  }
+
+  val resAll2 = client.execute{
+    search("zaj" / "aa") matchAllQuery()
+  }.await
+
+  val respon: Seq[RawStream] = resAll2.to[RawStream]
+
+
+
+  println(respon)
+
+
+  //println(hsl)
+}
--- src/main/scala/Elas5.scala
+++ src/main/scala/Elas5.scala
@@ -1,56 +1,36 @@
-import com.sksamuel.elastic4s.{ElasticsearchClientUri, TcpClient}
+import com.sksamuel.elastic4s.{ElasticsearchClientUri, Hit, HitReader, TcpClient}
 import com.sksamuel.elastic4s.http.ElasticDsl._
 import org.elasticsearch.action.support.WriteRequest.RefreshPolicy
 import com.sksamuel.elastic4s.http.HttpClient
 import org.apache.http.client.config.RequestConfig
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
-import com.sksamuel.elastic4s.HitReader
 import com.sksamuel.elastic4s.searches.queries.matches.MatchQueryBuilder
 
 
 //cari pembenaran dari import yang masih merah
 
 
-object Elas1 extends App {
+object Elas5 extends App {
   // Here we create an instance of the TCP client
   val client = HttpClient(ElasticsearchClientUri("localhost",9200),(requestConfigBuilder: RequestConfig.Builder) => { requestConfigBuilder.setConnectionRequestTimeout(60000) .setSocketTimeout(60000) .setConnectTimeout(60000) }, (httpClientBuilder: HttpAsyncClientBuilder) => { httpClientBuilder })
 
-
-  // await is a helper method to make this operation synchronous instead of async
-  // You would normally avoid doing this in a real program as it will block your thread
-//  client.execute {
-//    indexInto("bands" / "artists") fields ("name" -> "Jazzy") refresh(RefreshPolicy.IMMEDIATE)
-//  }.await
-
-// searc matchAll
-  val resAll = client.execute{
-    search("pjs" / "pj") matchAllQuery()
-  }.await
-
-  val resAll2 = client.execute{
-    search("zaj" / "aa") matchAllQuery()
-  }.await
-
+  case class ABC(aa: String, bb: String)
+  implicit object ABCHitReader extends HitReader[ABC] {
+    override def read(hit: Hit): Either[Throwable, ABC] = {
+      Right(ABC(hit.sourceAsMap("aa").toString, hit.sourceAsMap("bb").toString))
+    }
+  }
 
   // now we can search for the document we just indexed
   val resp = client.execute {
     search("zox" / "test1") query "lalal"
   }.await
+  val abc_new: Seq[ABC] = resp.to[ABC]
 
 
 
-  //search : Hitreader, coba lihat sosmed elasticsearch bagian hit reader, type class
 
-  val hasil = resAll2.hits.hits.toList
-  //val hasil2
-
-  val hsl = resAll2.hits.hits.map {
-    tr=>
-      val ddd = tr.sourceAsMap
-      println(ddd)
-  }
-
- // println(hasil)
+   println(abc_new)
 
 
   //println(hsl)
